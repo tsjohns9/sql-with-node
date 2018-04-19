acc6b11f6cd3080f096e132455a48e2325c39ede
Trevor Johnson <johnsontrevor55@gmail.com>
2018-04-19 10:05:56 -0700

    all manager searches work now

--- bamazonManager.js
+++ bamazonManager.js
@@ -42,10 +42,7 @@ function viewProducts() {
   var query =
     "SELECT item_id AS 'ID', product_name AS 'Name', price AS 'Price', stock_quantity AS 'Quantity' FROM products;";
   connection.query(query, function(err, res) {
-    if (err) {
-      console.log(err);
-      return connection.end();
-    }
+    if (err) throw err;
 
     // adds each item to resArray
     for (var i = 0; i < res.length; i++) {
@@ -61,10 +58,7 @@ function lowInventory() {
     "SELECT item_id AS 'ID', product_name AS 'Name', price AS 'Price', stock_quantity AS 'Quantity' FROM products WHERE stock_quantity < 5 ORDER BY stock_quantity ASC;";
 
   connection.query(query, function(err, res) {
-    if (err) {
-      console.log(err);
-      return connection.end();
-    }
+    if (err) throw err;
 
     for (var i = 0; i < res.length; i++) {
       table.push([res[i].ID, res[i].Name, "$" + res[i].Price, res[i].Quantity]);
@@ -74,34 +68,103 @@ function lowInventory() {
   connection.end();
 }
 
-function addInventory() {}
+function addInventory() {
+  inquirer
+    .prompt([
+      {
+        name: "id",
+        message:
+          "Enter the product ID of the item you would like to buy more of \n",
+        type: "prompt"
+      },
+      {
+        name: "quantity",
+        message: "How many would you like to buy?",
+        type: "prompt"
+      }
+    ])
+    .then(function(ans) {
+      getQuantity(ans.id, ans.quantity);
+    });
+}
 
 function addNewProduct() {
-  inquirer.prompt([
+  inquirer
+    .prompt([
+      {
+        name: "product",
+        message: "Enter the new product name \n",
+        type: "prompt"
+      },
+      {
+        name: "price",
+        message: "Enter the price of the new product \n",
+        type: "prompt"
+      },
+      {
+        name: "department",
+        message: "What department will this be in? \n",
+        type: "prompt"
+      },
+      {
+        name: "inventory",
+        message: "How many would you like to add? \n",
+        type: "prompt"
+      }
+    ])
+    .then(function(ans) {
+      var query = "INSERT INTO products SET ?";
+      connection.query(
+        query,
+        {
+          product_name: ans.product,
+          department_name: ans.department,
+          price: ans.price,
+          stock_quantity: ans.inventory
+        },
+        function(err, res) {
+          if (err) throw err;
+          viewProducts();
+        }
+      );
+    });
+}
+
+// gets the current quantity of an item
+function getQuantity(id, totalPurchased) {
+  var query = "SELECT stock_quantity FROM products WHERE ?";
+  connection.query(
+    query,
     {
-      name: "product",
-      message: "Enter the new product name \n",
-      type: "prompt"
+      item_id: id
     },
-    {
-      name: "price",
-      message: "Enter the price of the new product \n",
-      type: "prompt"
-    },
-    {
-      name: "department",
-      message: "What department will this be in? \n",
-      type: "prompt"
-    },
-    {
-      name: "inventory",
-      message: "How many would you like to add? \n",
-      type: "prompt"
+    function(err, res) {
+      if (err) throw err;
+      console.log(res);
+      updateInventory(id, res[0].stock_quantity, totalPurchased);
     }
-  ]);
-  // var query = 'INSERT INTO products SET ?';
+  );
+}
 
-  // connection.query(query, {
+// updates an item with its current quantity + the new total amount purchased
+function updateInventory(id, currentQ, totalPurchased) {
+  console.log(id, currentQ, totalPurchased);
+  var newQ = Number(currentQ) + Number(totalPurchased);
 
-  // })
+  var query = "UPDATE products SET ? WHERE ?";
+  connection.query(
+    query,
+    [
+      {
+        stock_quantity: newQ
+      },
+      {
+        item_id: id
+      }
+    ],
+    function(err, res) {
+      if (err) throw err;
+      viewProducts();
+    }
+  );
 }
